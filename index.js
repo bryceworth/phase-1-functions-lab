e2decf7e7f1e11ead7f8f4dc12dd22998d79f4c2
bryceworth <bryceaworth@gmail.com>
2022-03-24 10:51:26 -0700

    commit tree 6c3107383dc6f37ed2cc39a4d6b68dc770cc24b8

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks (number){
+    if (number > 42){
+    return number - headquarters;
+    }
+    else if (number < 42){
+        return headquarters - number;
+    }}
+
+function distanceFromHqInFeet (number){
+    return distanceFromHqInBlocks(number) * feet;
+    
+}
+
+function distanceTravelledInFeet (headquarters, num2){
+    if (num2 > headquarters){
+        return (num2 - headquarters)*feet;
+
+    }
+    else{
+    return (headquarters - num2)*feet;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 400 &&
+        distanceTravelledInFeet(start, destination) < 2000
+    ) {
+        return 2.56;
+    }
+    if (
+        distanceTravelledInFeet(start, destination) > 2000 && 
+        distanceTravelledInFeet(start, destination) < 2500
+    ) {
+        return 25;
+    }
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far";
+}
+} 
